4da1ac871dd81aaa3c9ad8ca40f66b085fca0d41
Dmytro Maliovanyi <str1k6r2001@gmail.com>
2019-09-12 12:47:39 +0300

    'removed redundant comments from Applicatio class'

--- src/main/java/parser/Application.java
+++ src/main/java/parser/Application.java
@@ -14,27 +14,10 @@ public class Application{
 
     public static boolean consoleWriterMode;
 
-
-
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class,args);
 
     }
 
-   /* @Override
-    public void run(String... args) throws Exception {
-
-        try {
-            consoleWriterMode = (args[0].equals("true") || args[0].equals("console")) || (Integer.parseInt(args[0]) == 1) ? true : false;
-        } catch (ArrayIndexOutOfBoundsException e){
-            consoleWriterMode = false;
-            System.out.println("\n\n\n\n\nNo arguments were specified so consoleWriterMode set to false by default\n\n\n\n\n\n");
-        }
-
-if (consoleWriterMode){
-    System.out.println("\n\n\n\n\n\n\n\n\n\nConsoleWriterMode set to true\n" +
-            "You will be able to see intermediate and while-processing data\n\n\n\n\n\n\n\n\n\n");
-}
-    }*/
 }
